86c205661f762a9674c730d459772d4c6ca900ff
Johnsd11 <johnsdennis17@gmail.com>
2020-12-17 23:38:00 -0600

    Fixed paths for many pictures. As well as made progress on projects section.

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function toggleText1(){
+    var text = document.getElementById("dropdown1");
+  if (text.style.display === "none") {
+    
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText2(){
+  var text = document.getElementById("dropdown2");
+if (text.style.display === "none") {
+  text.style.display = "block";
+} else {
+  text.style.display = "none";
+}
+}
+
+
+function toggleText3(){
+  var text = document.getElementById("dropdown3");
+if (text.style.display === "none") {
+  text.style.display = "block";
+} else {
+  text.style.display = "none";
+}
+}
+
+
+function toggleText4(){
+  var text = document.getElementById("dropdown4");
+if (text.style.display === "none") {
+  text.style.display = "block";
+} else {
+  text.style.display = "none";
+}
+}
